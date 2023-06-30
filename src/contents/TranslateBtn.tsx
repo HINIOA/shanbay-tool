28740fa054e950274a88317b1f6919e08840b9ce
Qiu Xinyao <xinyao.qiu@shopee.com>
2023-06-30 16:14:03 +0800

    feat(app): 实现主渲染逻辑

--- src/contents/TranslateBtn.tsx
+++ src/contents/TranslateBtn.tsx
@@ -1,61 +1,29 @@
-import cssText from "data-text:~/contents/style.css"
-import { debounce } from "lodash"
-import type { PlasmoCSUIJSXContainer, PlasmoRender } from "plasmo"
-import type { FC } from "react"
-import { createRoot } from "react-dom/client"
+import type { PlasmoCSUIJSXContainer, PlasmoRender } from "plasmo";
+import type { ButtonHTMLAttributes, FC } from "react";
 
-interface Position {
-  left: number
-  top: number
+export interface Position {
+  left: number;
+  top: number;
 }
 
-const TranslateBtn: FC<{ position?: Position }> = ({ position }) => {
-  const { left, top } = position || {}
+interface TranslateBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  position: Position;
+}
+
+const TranslateBtn: FC<TranslateBtnProps> = ({ position, ...extra }) => {
+  const { left, top } = position || {};
   return (
     <button
       className="translate-btn"
-      style={{ position: "absolute", left, top }}>
+      style={{ position: "fixed", left, top }}
+      {...extra}
+    >
       扇贝翻译
     </button>
-  )
-}
+  );
+};
 
-export default TranslateBtn
+export default TranslateBtn;
 
-export const getStyle = () => {
-  const style = document.createElement("style")
-  style.textContent = cssText
-  return style
-}
-
-export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async ({
-  anchor,
-  createRootContainer
-}) => {
-  const rootContainer = await createRootContainer(anchor)
-  const root = createRoot(rootContainer)
-
-  function handleSelect() {
-    const selection = window.getSelection()
-
-    if (selection.rangeCount < 1) {
-      return
-    }
-
-    const range = selection.getRangeAt(0)
-    const rect = range.getBoundingClientRect()
-    const word = selection.toString()
-
-    word &&
-      root.render(
-        <TranslateBtn position={{ left: rect.left, top: rect.bottom }} />
-      )
-
-    // 点击外侧关闭弹框
-    setTimeout(() => {
-      document.addEventListener("click", () => root.render(null))
-    }, 500)
-  }
-
-  document.addEventListener("selectionchange", debounce(handleSelect, 500))
-}
+// 取消自动渲染
+export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async () => {};
